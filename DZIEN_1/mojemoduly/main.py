8f6b56d7268b89bc9cf12c81e190cbc999f16cea
albim72 <marcin.albiniak72@gmail.com>
2024-09-24 12:08:36 +0200

    Update main.py

--- DZIEN_1/mojemoduly/main.py
+++ DZIEN_1/mojemoduly/main.py
@@ -3,8 +3,14 @@
 from dane import nrfilii as nf
 from dane import book as bk
 
+from funkcje.funkcja_kol import czytaj_liste, czytaj_slownik
+
 #kopiowanie linii/bloku tekstu -> CTRL+D
 print("to jest pierwszy program w PyCharm")
 print("___ kolekcje z modułu: dane ___")
 print(nf)
 print(bk)
+
+print("użycie funkcji do kolekcji..........")
+czytaj_liste(nf)
+czytaj_slownik(bk)
