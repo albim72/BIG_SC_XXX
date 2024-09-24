61394bf46dccece3587aa01ae48a726394c23c2c
albim72 <marcin.albiniak72@gmail.com>
2024-09-24 11:54:03 +0200

    Create main.py

---
+++ DZIEN_1/mojemoduly/main.py
@@ -0,0 +1,10 @@
+# import dane
+# import dane as dn
+from dane import nrfilii as nf
+from dane import book as bk
+
+#kopiowanie linii/bloku tekstu -> CTRL+D
+print("to jest pierwszy program w PyCharm")
+print("___ kolekcje z modułu: dane ___")
+print(nf)
+print(bk)
