65b20a1ee5a2a8b0ea50f553c452e56f4875398b
albim72 <marcin.albiniak72@gmail.com>
2024-09-25 12:19:25 +0200

    Update main.py

--- DZIEN_2/prosta_siec_neuronowa/main.py
+++ DZIEN_2/prosta_siec_neuronowa/main.py
@@ -3,3 +3,17 @@ from simplenn import SimpleNeuralNetwork
 
 network = SimpleNeuralNetwork()
 print(network)
+
+train_inputs = np.array([[1,1,0],[1,1,1],[1,1,0],[1,0,0],[0,1,1],[0,1,0],[0,0,0]])
+train_outputs = np.array([[1,0,1,1,0,1,1]]).T
+train_iters = 50_000
+
+network.train(train_inputs,train_outputs,train_iters)
+print("wektor wag po wytrenowaniu sieci...")
+print(network.weights)
+
+print("ocena modelu...")
+test_data = np.array([[1,1,1],[1,0,0],[0,1,1],[0,1,0],[0,0,1],[0,0,0],])
+
+for data in test_data:
+    print(f"wynik predykcji dla {data} wynosi: {network.propagation(data)}")
