23e50a255a8b98cfffdbe06d2e72bce77ffb860c
albim72 <marcin.albiniak72@gmail.com>
2024-09-25 11:57:04 +0200

    Update simplenn.py

--- DZIEN_2/prosta_siec_neuronowa/simplenn.py
+++ DZIEN_2/prosta_siec_neuronowa/simplenn.py
@@ -4,3 +4,32 @@ class SimpleNeuralNetwork:
     def __init__(self):
         np.random.seed(1)
         self.weights = 2*np.random.random((3,1))-1
+
+    def __repr__(self):
+        return f"obiekt klasy {self.__class__.__name__}\nwagi:\n {self.weights}"
+
+    # def __str__(self):
+    #     return f"zastępstwo repr"
+    # def __new__(cls, *args, **kwargs):
+    #     return object.__new__(SimpleNeuralNetwork)
+    
+    def sigmoid(self,x):
+        return 1/(1+np.exp(-x))
+    
+    def d_sigmoid(self,x):
+        return x*(1-x)
+    
+    #funkcja propagacji
+    def propagation(self,inputs):
+        return self.sigmoid(np.dot(inputs.astype(float),self.weights))
+    
+    #funkcja propagacji wstecznej
+    def backward_propagation(self,propagation_result,train_input,train_output):
+        error = train_output - propagation_result
+        self.weights += np.dot(train_input.T,error*self.d_sigmoid(propagation_result))
+        
+    #trening
+    def train(self,train_input,train_output,train_iters):
+        for _ in range(train_iters):
+            propagation_result = self.propagation(train_input)
+            self.backward_propagation(propagation_result,train_input,train_output)
